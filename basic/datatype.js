c26789d393ed9db15eb4384a1b64618de8936919
Anuj Bhadoria <153363575+0AnujBhadoria0@users.noreply.github.com>
2026-09-07 15:53:24 +0000

    datatypes done

---
+++ basic/datatype.js
@@ -0,0 +1,23 @@
+"use strict"// treat all JS code as newer version 
+// alert (3+3) //didnt work because we are using node js not browser 
+let name = "Anuj"//this is string
+let age = 19// this is number
+let isLoggedIn = true //this is boolean(true/false)
+let state = null // zero or empty
+
+// nummber range => 2 to power of 53
+// bigint => use only for big or large number
+// string => "" or ''
+// boolean => true or false
+// null => standalone value
+// undefined => value not assigned
+/// symbol => to find uniqeness we use symbols
+// object => 
+
+// console.log(typeof (of which type we want to know))    
+
+console.log(typeof age);// number
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+console.log(typeof "Anuj");// string
+console.log(typeof true);// boolean
